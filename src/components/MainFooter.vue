14f7e03434e73b809290601c2b515bc2a99bfe02
shawn.thorne9 <shawn.thorne9@gmail.com>
2025-03-26 00:27:12 -0600

    updates footer with links

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -4,16 +4,26 @@
       <div>
         <div class="text-3xl font-bold">Shawn Thorne</div>
         <div class="font-bold">Web Developer</div>
-        <div>Github link</div>
+        <div>
+          <a class="link" target="_blank" href="https://github.com/ShawnThorne">Github Link</a>
+        </div>
       </div>
       <div class="lg:text-right">
-        Designed in <span class="font-bold">Figma</span>.
+        Designed in
+        <a target="_blank" href="https://www.figma.com/" class="font-bold link-hover">Figma</a>.
         <br />
-        Made with <span class="font-bold">Vue</span>, <span class="font-bold">Tailwind CSS</span>,
-        and <span class="font-bold">DaisyUi</span>.
+        Made with <a target="_blank" href="https://vuejs.org/" class="font-bold link-hover">Vue</a>,
+        <a target="_blank" href="https://tailwindcss.com/" class="font-bold link-hover"
+          >Tailwind CSS</a
+        >, and
+        <a target="_blank" href="https://daisyui.com/" class="font-bold link-hover">DaisyUi</a>.
         <br />
-        Deployed using self-hosted <span class="font-bold">Coolify</span> via
-        <span class="font-bold">Hostinger</span> services.
+        Deployed using self-hosted
+        <a target="_blank" href="https://coolify.io/" class="font-bold link-hover">Coolify</a> via
+        <a target="_blank" href="https://www.hostinger.com/" class="font-bold link-hover"
+          >Hostinger</a
+        >
+        services.
       </div>
     </div>
   </div>
